f08956aecd27516058c967d730e98284e3114b46
unknown <shimada.henrique@gmail.com>
2020-08-09 12:08:30 -0300

    Adding insertion Statement based on item.id

---
+++ src/InsertionTeste.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class InsertionTeste {
+
+	public static void main(String[] args) throws SQLException {
+		ConnectionFactory factory = new ConnectionFactory();
+		Connection connection = factory.retrieveConnection();
+		
+		Statement statement = connection.createStatement();
+		String sql = "INSERT INTO PRODUTO (nome, descricao) VALUES ('Mouse', 'Mouse sem fio')";
+
+		/**
+		 * test if the query passes
+		 * expects: false, since  there's no information retrieved in the resultSet attribute
+		 */
+		/*
+		boolean result = statement.execute(sql);
+		System.out.println(result);
+		*/
+		
+		/**
+ 		 * However, information was registered. so the following application is better fit
+		 */
+		statement.execute(sql, Statement.RETURN_GENERATED_KEYS);
+		ResultSet rs = statement.getGeneratedKeys();
+		while(rs.next()) {
+			Integer id = rs.getInt(1); // in SQL, the first item of a list is 1
+			System.out.println("O id criado foi: "+ id);
+		}
+
+	}
+
+}
