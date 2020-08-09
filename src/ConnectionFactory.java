cd0e182ad13016c1655b6f9daea72e9ac08b1538
unknown <shimada.henrique@gmail.com>
2020-08-09 11:45:30 -0300

    Separating responsibity by creation of ConnectionFactory

---
+++ src/ConnectionFactory.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectionFactory {
+	public Connection retrieveConnection() throws SQLException {
+		/**
+		 * Creating the connection
+		 */
+		Connection connection = DriverManager.getConnection(
+				"jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "1234");
+		
+		return connection;
+	}
+
+}
